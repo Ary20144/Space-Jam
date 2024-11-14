6e1866048ce0286323330b91f4216dd469eb2753
Matthew Lee <matthew.c.lee@gmail.com>
2024-11-13 20:22:41 -0800

    Matthew Test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { diffuseColor } from 'three/webgpu';
 import { distance } from 'three/webgpu';
-
+//test
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 10, 20);
